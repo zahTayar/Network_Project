3b2cad490193984e1b71369da3a6d6412328717c
lidar1996 <lidar602@gmail.com>
2022-05-18 20:00:34 +0300

    update

---
+++ src/main/network/helpers/config.py
@@ -0,0 +1,6 @@
+class config:
+    password_count = 10
+    password_structure = "Big Small Digits Special"
+    history = 3
+    word_bank = "admin ronaldo"
+    login_tries = 3
--- src/main/network/helpers/password_checker.py
+++ src/main/network/helpers/password_checker.py
@@ -1,4 +1,5 @@
-import configparser
+from src.main.network.helpers.config import config
+import re
 
 
 class password_checker:
@@ -8,15 +9,32 @@ class password_checker:
         self.pull_from_config_file()
 
     def pull_from_config_file(self):
-        config = configparser.ConfigParser()
-        password_requirments = config.read('configurations.ini')['password']
-        self.password_count = password_requirments['password_count']
-        for item in password_requirments['password_count'].split(' '):
+        self.password_count = config.password_count
+        for item in config.password_structure.split(' '):
             self.password_structure.append(item)
-        self.history = password_requirments['history']
-        for item in password_requirments['word_bank'].split(' '):
+        self.history = config.history
+        for item in config.word_bank.split(' '):
             self.word_bank.append(item)
-        self.login_tries = password_requirments['login_tries']
+        self.login_tries = config.login_tries
 
-    def check_password(self):
-        pass
+    def check_password(self, password):
+        if len(password) > self.password_count:
+            raise Exception("password digits less than" + self.password_count)
+        for item in self.password_structure:
+            if not self.check_structure(item, password):
+                raise Exception("missing requirments")
+        for word in self.word_bank:
+            if word == password:
+                raise Exception("password in word bank")
+
+
+    def check_structure(self, item, password):
+        return {
+            "Big": any(password.isupper() for ele in password),
+            "Small": any(password.islower() for ele in password),
+            "Digit": any(password.isdigit() for ele in password),
+            "Special": bool(re.match('^[a-zA-Z0-9]*$', password)) == True
+        }
+
+
+password_checker = password_checker()
